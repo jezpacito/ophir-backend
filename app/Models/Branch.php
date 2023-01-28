5170c5421bbc629a7a5cfc6c1abe3553aaf44d63
jezreelgatmaitan-cp <jezreel.gatmaitan@coreproc.com>
2023-01-28 15:06:03 +0800

    added ide helper

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -5,6 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Branch
+ *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\User[] $users
+ * @property-read int|null $users_count
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Branch newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Branch newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Branch query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Branch whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Branch whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Branch whereUpdatedAt($value)
+ *
+ * @mixin \Eloquent
+ */
 class Branch extends Model
 {
     use HasFactory;
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,97 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property int|null $role_id
+ * @property int|null $branch_id
+ * @property string $account_type
+ * @property string $userame
+ * @property string $lastname
+ * @property string $firstname
+ * @property string|null $middlename
+ * @property string|null $gender
+ * @property string|null $birthdate
+ * @property int|null $age
+ * @property string|null $address
+ * @property string|null $postal_code
+ * @property string|null $contact_no
+ * @property string|null $civil_status
+ * @property string|null $height
+ * @property string|null $weigth
+ * @property string|null $citizenship
+ * @property string|null $sponsor
+ * @property string|null $sss_number
+ * @property string|null $tin_number
+ * @property string|null $ben_firstname
+ * @property string|null $ben_middlename
+ * @property string|null $ben_lastname
+ * @property string|null $ben_relationship
+ * @property string|null $ben_birthdate
+ * @property string|null $status
+ * @property string|null $facebook
+ * @property string|null $messenger
+ * @property string|null $twitter
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Branch|null $branch
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \App\Models\Role|null $role
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Staff[] $staffs
+ * @property-read int|null $staffs_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Sanctum\PersonalAccessToken[] $tokens
+ * @property-read int|null $tokens_count
+ *
+ * @method static \Database\Factories\UserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereAccountType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereAge($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereBenBirthdate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereBenFirstname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereBenLastname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereBenMiddlename($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereBenRelationship($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereBirthdate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereBranchId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCitizenship($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCivilStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereContactNo($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereFacebook($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereFirstname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereGender($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereHeight($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLastname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereMessenger($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereMiddlename($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePostalCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRoleId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereSponsor($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereSssNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereTinNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereTwitter($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUserame($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereWeigth($value)
+ *
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
