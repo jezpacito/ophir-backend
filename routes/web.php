0d7b75245a6ceceb8a519d5a9a92047f064d4c93
jezreelgatmaitan-cp <jezreel.gatmaitan@coreproc.com>
2023-03-17 17:54:24 +0800

    clear routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Models\User;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,16 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('test', function () {
-    $userCreated = User::first()->created_at->toDateString();
-    $userCreateDateParse = Carbon::parse($userCreated);
-    // dd($userCreateDateParse);
-    // dd($userCreatedDate);
-    $lastMonthDate = now()->subMonth()->toDateString();
-    $lastMonthDateParse = Carbon::parse($lastMonthDate);
-
-    $result = $userCreateDateParse->eq($lastMonthDateParse);
-    dd($result);
 });
